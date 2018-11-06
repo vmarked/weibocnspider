3182670a0340fc7d14668b8f529bb9f7e33ec265
vmarked <greensuu@outlook.com>
2018-11-06 16:55:52 +0800

    通过管道对微博时间进行清洗处理, 统一将 crawled_at 赋值为当前时间

--- weibo/weibo/pipelines.py
+++ weibo/weibo/pipelines.py
@@ -4,8 +4,45 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
+import re
+import time
+import logging
+import pymongo
+
+from weibo.items import *
+
+# 统一将 crawled_at 赋值为当前时间
+class TimePipeline(object):
+    def process_item(self, item, spider):
+        if isinstance(item, UserItem) or isinstance(item, WeiboItem):
+            now = time.strftime('%Y-%m-%d %H:%M', time.localtime())
+            item['crawled_at'] = now
+        return item
 
 
 class WeiboPipeline(object):
+
     def process_item(self, item, spider):
+        if isinstance(item, WeiboItem):
+            if item.get('created_at'):
+                item['created_at'] = item['created_at'].strip()
+                item['created_at'] = self.parse_time(item.get('created_at'))
+            if item.get('pictures'):
+                item['pictures'] = [pic.get('url') for pic in item.get('pictures')]
         return item
+
+    def parse_time(self, date):
+        if re.match('刚刚', date):
+            date = time.strftime('%Y-%m-%d %H:%M', time.localtime(time.time()))
+        if re.match('\d+分钟前', date):
+            minute = re.match('(\d+)', date).group(1)
+            date = time.strftime('%Y-%m-%d %H:%M', time.localtime(time.time() - float(minute) * 60))
+        if re.match('\d+小时前', date):
+            hour = re.match('(\d+)', date).group(1)
+            date = time.strftime('%Y-%m-%d %H:%M', time.localtime(time.time() - float(hour) * 60 * 60))
+        if re.match('昨天.*', date):
+            date = re.match('昨天(.*)', date).group(1).strip()
+            date = time.strftime('%Y-%m-%d', time.localtime() - 24 * 60 * 60) + ' ' + date
+        if re.match('\d{2}-\d{2}', date):
+            date = time.strftime('%Y-', time.localtime()) + date + ' 00:00'
+        return date
