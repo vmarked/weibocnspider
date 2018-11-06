5e53ba4e509d7b61b7a344c9ed49de595ea281fb
vmarked <greensuu@outlook.com>
2018-11-06 16:57:37 +0800

    添加管道进行数据存储

--- weibo/weibo/pipelines.py
+++ weibo/weibo/pipelines.py
@@ -21,16 +21,6 @@ class TimePipeline(object):
 
 
 class WeiboPipeline(object):
-
-    def process_item(self, item, spider):
-        if isinstance(item, WeiboItem):
-            if item.get('created_at'):
-                item['created_at'] = item['created_at'].strip()
-                item['created_at'] = self.parse_time(item.get('created_at'))
-            if item.get('pictures'):
-                item['pictures'] = [pic.get('url') for pic in item.get('pictures')]
-        return item
-
     def parse_time(self, date):
         if re.match('刚刚', date):
             date = time.strftime('%Y-%m-%d %H:%M', time.localtime(time.time()))
@@ -46,3 +36,49 @@ class WeiboPipeline(object):
         if re.match('\d{2}-\d{2}', date):
             date = time.strftime('%Y-', time.localtime()) + date + ' 00:00'
         return date
+
+
+    def process_item(self, item, spider):
+        if isinstance(item, WeiboItem):
+            if item.get('created_at'):
+                item['created_at'] = item['created_at'].strip()
+                item['created_at'] = self.parse_time(item.get('created_at'))
+            if item.get('pictures'):
+                item['pictures'] = [pic.get('url') for pic in item.get('pictures')]
+        return item
+
+
+class MongoPipeline(object):
+    def __init__(self, mongo_uri, mongo_db):
+        self.mongo_uri = mongo_uri
+        self.mongo_db = mongo_db
+
+    @classmethod
+    def from_crawler(cls, crawler):
+        return cls(
+            mongo_uri=crawler.settings.get('MONGO_URI'),
+            mongo_db=crawler.settings.get('MONGO_DATABASE')
+        )
+
+    def open_spider(self, spider):
+        self.client = pymongo.MongoClient(self.mongo_uri)
+        self.db = self.client[self.mongo_db]
+        self.db[UserItem.collection].create_index([('id', pymongo.ASCENDING)])
+        self.db[WeiboItem.collection].create_index([('id', pymongo.ASCENDING)])
+
+    def close_spider(self, spider):
+        self.client.close()
+
+    def process_item(self, item, spider):
+        if isinstance(item, UserItem) or isinstance(item, WeiboItem):
+            self.db[item.collection].update({'id': item.get('id')}, {'$set': item}, True)
+        if isinstance(item, UserRelationItem):
+            self.db[item.collection].update(
+                {'id': item.get('id')},
+                {'$addToSet':
+                    {
+                        'follows': {'$each': item['follows']},
+                        'fans': {'$each': item['fans']}
+                    }
+                }, True)
+        return item
